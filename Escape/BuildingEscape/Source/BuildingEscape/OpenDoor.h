c20f3a61605f7b0232ab04984b6da828494c7fcf
TimSAS <34079684+TimSAS@users.noreply.github.com>
2018-01-16 22:51:35 -0500

    Using GetTimeSeconds()
    Lecture 73 Creating a delay for doors to close after you trigger the volume

--- Escape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ Escape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -22,18 +22,24 @@ public:
 	virtual void BeginPlay() override;
 
 	void OpenDoor();
+	void CloseDoor();
 
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 
 private:
-	UPROPERTY(VisibleAnywhere)
-		float OpenAngle = 90.0f;
+	UPROPERTY(EditAnywhere)
+	float OpenAngle = 70.0f;
 
 	UPROPERTY(EditAnywhere)
-		ATriggerVolume* PressurePlate;
+	ATriggerVolume* PressurePlate;
 
+	UPROPERTY(EditAnywhere)
+	float DoorCloseDelay = 1.0f;
+
+	float LastDoorOpenTime;
 	
-		AActor* ActorThatOpens;  //remember pawn inherits from the actor
+	AActor* ActorThatOpens;  //remember pawn inherits from the actor
+	AActor* Owner; // The owning door
 };
