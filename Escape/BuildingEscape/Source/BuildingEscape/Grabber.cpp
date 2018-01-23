4bbe745da77d68a6dff28a872ddef79e48ee6e85
TimSAS <34079684+TimSAS@users.noreply.github.com>
2018-01-22 23:32:57 -0500

    Accessors & Memory Layout
    Lecture 85, binding "release" functionality to our "grab" input component

--- Escape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ Escape/BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -34,6 +34,7 @@ void UGrabber::BeginPlay()
 		UE_LOG(LogTemp, Warning, TEXT("Input component found"));
 		/// Bind the input axis
 		InputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		InputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
 	}
 	else {
 		UE_LOG(LogTemp, Error, TEXT("%s missing input component"), *GetOwner()->GetName());
@@ -44,6 +45,11 @@ void UGrabber::BeginPlay()
 void UGrabber::Grab()
 {
 	UE_LOG(LogTemp, Warning, TEXT("Grab pressed"));
+}
+
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Grab released"));
 }
 
 // Called every frame
