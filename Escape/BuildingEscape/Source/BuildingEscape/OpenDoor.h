2b5afed060f9d460fbfd0f68d83e22bbdb6ee4db
TimSAS <34079684+TimSAS@users.noreply.github.com>
2018-01-26 00:16:53 -0500

    Everything in its place
    Lecture 96

--- Escape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ Escape/BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -9,7 +9,7 @@
 #include "OpenDoor.generated.h"
 
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
 
 
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
@@ -24,28 +24,24 @@ public:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	void OpenDoor();
-	void CloseDoor();
-
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UPROPERTY(BlueprintAssignable)
-	FOnOpenRequest OnOpenRequest;
+	FDoorEvent OnOpen;
+
+	UPROPERTY(BlueprintAssignable)
+	FDoorEvent OnClose;
 
 private:
 	UPROPERTY(EditAnywhere)
-	float OpenAngle = 70.0f;
-
-	UPROPERTY(EditAnywhere)
 	ATriggerVolume* PressurePlate = nullptr;
-
-	UPROPERTY(EditAnywhere)
-	float DoorCloseDelay = 1.0f;
-
-	float LastDoorOpenTime;
 	
-	AActor* Owner = nullptr; // The owning door
+	UPROPERTY(EditAnywhere)
+	float TriggerMass = 30.f;
+
+	// The owning door
+	AActor* Owner = nullptr; 
 
 	// returns total mass on a plate
 	float GetTotalMassOfActorsOnPlate();
